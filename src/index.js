40049fe01d5d1ff6fdcd93440b6cf67622458e78
Nicola Sartorato <niksart@airmail.cc>
2018-12-16 16:01:00 +0100

    Implement entry point for apis

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const string_square = require('./string_square');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json('Hello World!');
+});
+
+app.get('/square', (req, res) => {
+    res.json({result: string_square(req.query.string)});
+});
+
+app.listen(PORT, () => console.log('App listening on port ' + PORT));
